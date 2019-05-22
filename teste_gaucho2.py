ed2f1542c7d03799340111e8764818386a71ae32
Marcelopalinux <50153085+Marcelopalinux@users.noreply.github.com>
2019-05-22 09:38:51 -0300

    Update teste_gaucho2.py
    capturar imagem do ronaldinho gaucho utilizando utilizando os modulos requests e bs4 ( beautiful soup )

--- teste_gaucho2.py
+++ teste_gaucho2.py
@@ -1,79 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import requests
 import bs4
-
-
-# In[2]:
-
 
 res = requests.get('https://pt.wikipedia.org/wiki/Ronaldinho_Ga%C3%BAcho')
 
 
-# In[3]:
-
-
 soap = bs4.BeautifulSoup(res.text,'lxml')
-
-
-# In[29]:
-
 
 img1 = soap.select('.image')[0]
 
-
-# In[30]:
-
-
 img2 = img1.find_all('img')
 
-
-# ## 
-
-# In[31]:
-
-
 imagem_ronaldinho = 'http:'+img2[0]['src']
-
-
-# In[32]:
-
 
 imagem_ronaldinho
 
 
-# In[20]:
-
-
 resronaldo = requests.get(imagem_ronaldinho)
 
-
-# In[23]:
-
-
 file_gaucho = open('gaucho3.png','wb')
-
-
-# In[24]:
-
 
 file_gaucho.write(resronaldo.content)
 
 
-# In[25]:
-
-
 file_gaucho.close()
 
-
 # <img src="gaucho3.png">
-
-# In[ ]:
-
-
-
-
