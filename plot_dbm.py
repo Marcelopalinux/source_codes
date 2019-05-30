a40d0c98d592fa1dcdead5bb24a7ecf2a0469c52
Marcelopalinux <50153085+Marcelopalinux@users.noreply.github.com>
2019-05-30 13:31:48 -0300

    Add files via upload

--- plot_dbm.py
+++ plot_dbm.py
@@ -1,20 +1,32 @@
 import matplotlib.pyplot as plt
 import pyodbc
 
-cnxn = pyodbc.connect("DSN=DBINTERON")
-cursor = cnxn.cursor()
+try:
+    cnxn = pyodbc.connect("DSN=BANCOFORUM")
+    cursor = cnxn.cursor()
+    cursor.execute("select id_estado as 'UF',count(id_estado) from loja.cep group by id_estado order by 2 desc")
+    rows = cursor.fetchall()
+except:
+    print('error')
 
-cursor.execute("select id_estado as 'UF',count(id_estado) from cep group by id_estado order by 2 desc")
-rows = cursor.fetchall()
 x = []
 y = []
 
 for i in range(len(rows)):
     x.append(rows[i][0])
     y.append(rows[i][1])
-
+plt.rcParams.update({'font.size':5})
 plt.bar(x,y,align="center",color="r",width=0.9)
 #plt.scatter(x,y,color="y",linewidths=0.1)
 plt.title("Ceps por Estado")
-plt.show()
+#capturar valor largura e altura da saida
+fig_size = plt.rcParams["figure.figsize"]
+
+# aumentar o valor
+fig_size[0] = 12
+fig_size[1] = 9
+plt.rcParams["figure.figsize"] = fig_size
+
+#plt.show()
+plt.savefig('c:/tb/ceps_por_estado.pdf', format='pdf',orientation='landscape')
 
